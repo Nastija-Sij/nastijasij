cebb3878c765c6370fd1921d4ff0effb9f9949fa
Nastija <nastija.sij@gmail.com>
2025-08-04 17:35:42 +0200

    created JS file

---
+++ script.js
@@ -0,0 +1,8 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav.-link-style");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {})
+})
